f6789f36f6b92e934230ef16968a9a107eef1536
844805 <844805@LTIN198628.cts.com>
2020-07-06 16:54:06 +0530

    Commit

---
+++ Processapi/src/main/java/com/example/Processapi/ProcessapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Processapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessapiApplication.class, args);
+	}
+
+}
